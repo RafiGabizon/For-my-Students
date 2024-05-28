4f3d3b84be3aba13abb783b035421bea50d6248c
Rafi Gabizon <109092985+RafiGabizon@users.noreply.github.com>
2024-05-28 09:04:39 +0300

    assets for students+calc project .
    add some assets for students and the calc project we did together  .

---
+++ tkLearn.py
@@ -0,0 +1,66 @@
+from tkinter import *
+
+num = 0
+
+#-----------------פונקציות:-----------------------
+
+def whenClicked():
+    print("You clicked the sec button !")
+def whenClicked2():
+    num=int(numToConvert.get())
+    numLabel.config(text=num)
+    if num == 0 :
+        print("0")
+    else:
+        print("else")
+    return num
+
+#-------------תחילת יצירת החלון:-----------------
+
+window = Tk()
+
+window.title("my window")
+
+window.geometry("400x400+400+100")
+
+window.configure(bg="black")
+
+myString =Label(window,text="That is my first label",bg="light green") 
+
+myString.pack(pady=30)
+
+
+myBtn = Button(window,text="my first btn", command=lambda: print("You clicked the first button"))
+myBtn.pack(side=("bottom"))
+
+
+
+numToConvert = Entry(window)
+numToConvert.pack()
+
+
+numLabel = Label(window, text=num)
+numLabel.pack()
+
+
+myBtn2 = Button(window,text="my sec btn",command=whenClicked)
+myBtn2.pack(pady=50)
+
+
+myBtn3 = Button(window,text="click Here!",command=whenClicked2)
+myBtn3.pack(pady=50)
+
+myBtn4 = Button(window,text="click Here!",command=whenClicked2)
+myBtn3.pack(pady=50)
+
+txt = Text(window)
+txt.pack(pady=50)
+
+
+num=txt.get(END)
+
+
+window.mainloop()
+
+
+
